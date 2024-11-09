8cf04d5291bfc1d0b531e816d977b3a5adbae9aa
Pankaj chejara <pankajchejara23@gmail.com>
2024-11-09 10:27:52 +0200

    change static dir

--- taico_cotrack/settings.py
+++ taico_cotrack/settings.py
@@ -201,7 +201,7 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
