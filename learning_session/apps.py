53893edfb939f7417df8886897aad7e21245e670
Pankaj chejara <pankajchejara23@gmail.com>
2024-12-19 13:24:03 +0200

    commening load files code

--- learning_session/apps.py
+++ learning_session/apps.py
@@ -12,5 +12,5 @@ class ApiConfig(AppConfig):
     name = 'api'
     CQ_MODEL_FILE = os.path.join(settings.MEDIA_ROOT,"CQ_model_66percent.pkl")
     ARG_MODEL_FILE = os.path.join(settings.MEDIA_ROOT,"ARG_model_66percent.pkl")
-    cq_model = pickle.load(open(CQ_MODEL_FILE,'rb'))
-    arg_model = pickle.load(open(ARG_MODEL_FILE,'rb'))
+    #cq_model = pickle.load(open(CQ_MODEL_FILE,'rb'))
+    #arg_model = pickle.load(open(ARG_MODEL_FILE,'rb'))
