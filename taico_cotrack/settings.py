b83eae832bb165871a04750717bad3d43ec11735
Pankaj chejara <pankajchejara23@gmail.com>
2025-01-19 11:54:58 +0200

    adding static_root default

--- taico_cotrack/settings.py
+++ taico_cotrack/settings.py
@@ -200,12 +200,8 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-if DEBUG:
-    STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'static')
-       ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
