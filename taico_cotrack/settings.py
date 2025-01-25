b219a94844be06a96b8be5c4dcac3980ce0c8d41
Pankaj chejara <pankajchejara23@gmail.com>
2025-01-25 19:06:32 +0200

    using mysql db

--- taico_cotrack/settings.py
+++ taico_cotrack/settings.py
@@ -29,7 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-
 DEBUG = True
 
 ALLOWED_HOSTS = ["86.107.197.103","localhost","www.cotrack.website"]
@@ -140,7 +139,7 @@ WSGI_APPLICATION = "taico_cotrack.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
+"""
 DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
@@ -158,7 +157,7 @@ DATABASES = {
             'PORT': str(os.getenv('COTRACK_DB_PORT')),
         }
     }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
